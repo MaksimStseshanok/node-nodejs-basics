33d5576e33797f6b5a3931b5c25529620e20aab3
Maksim Stseshanok <m.stseshanok@andersenlab.com>
2022-12-04 13:28:49 +0300

    feat: add parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const res = [];
+  process.argv.forEach((prop, index) => {
+    if (prop.includes("--")) {
+      const propName = prop.slice(2);
+      res.push(`${propName} is ${process.argv[index + 1]}`);
+    }
+  });
+
+  console.log(res.join(", "));
 };
 
 parseArgs();
