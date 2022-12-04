f6a187c73bd1b5663bd4002da2e416e05bd11ece
Maksim Stseshanok <m.stseshanok@andersenlab.com>
2022-12-04 12:52:30 +0300

    feat: add parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const envKeys = Object.keys(process.env);
+
+  for (const key of envKeys) {
+    if (key.includes(prefix)) {
+      console.log(`${key}=${process.env[key]}`);
+    }
+  }
 };
 
 parseEnv();
