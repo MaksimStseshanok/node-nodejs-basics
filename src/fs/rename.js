91877c0bcf6fefe1197432d701d4b607c901aabe
Maksim Stseshanok <m.stseshanok@andersenlab.com>
2022-12-05 00:16:50 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./src/fs/files/wrongFilename.txt");
+    await fs.rename(
+      "./src/fs/files/wrongFilename.txt",
+      "./src/fs/files/properFilename.md"
+    );
+  } catch {
+    console.log("FS operation failed");
+  }
 };
 
 await rename();
