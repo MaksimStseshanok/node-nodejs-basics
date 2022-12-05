a59b47ecf4cdbe18b0b722458d3be655850fece9
Maksim Stseshanok <m.stseshanok@andersenlab.com>
2022-12-05 18:11:12 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import fs from "fs/promises";
+import { defaultPath, error } from "./constants.js";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(defaultPath);
+    files.forEach((file) => console.log(file));
+  } catch {
+    console.log(error);
+  }
 };
 
 await list();
